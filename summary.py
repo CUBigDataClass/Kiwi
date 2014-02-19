80b7bab05e148f0ba0b24df713131a8e1613e0e0
Gregory Petropoulos <gregpetrop@gmail.com>
2014-02-18 18:22:27 -0700

    added comments to summary.py

--- summary.py
+++ summary.py
@@ -1,9 +1,11 @@
 #/usr/bin/python
 
-import pandas as pd
-import numpy as np
-train = pd.read_csv("data/train.csv")
-print "Number of Rows: " + str(len(train))
-print "Number of Unique Users: " + str(len(train['user'].unique()))
+import pandas as pd                                                         #import pandas
+import numpy as np                                                          #import numpy
+
+train = pd.read_csv("data/train.csv")                                       #load train.csv as a pandas frame
+
+print "Number of Rows: " + str(len(train))                                  #prints the total number of rows
+print "Number of Unique Users: " + str(len(train['user'].unique()))         #prints the number of unique users...
 print "Number of Unique SKU: " + str(len(train['sku'].unique()))
 print "Number of Unique Categories: " + str(len(train['category'].unique()))
