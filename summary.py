c40d5122f94c405011e4d6914b744a8124d6003c
Gregory Petropoulos <gregpetrop@gmail.com>
2014-02-18 18:15:01 -0700

    Updated summary.py to print out total number of lines

--- summary.py
+++ summary.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 train = pd.read_csv("data/train.csv")
+print "Number of Rows: " + str(len(train))
 print "Number of Unique Users: " + str(len(train['user'].unique()))
 print "Number of Unique SKU: " + str(len(train['sku'].unique()))
 print "Number of Unique Categories: " + str(len(train['category'].unique()))
