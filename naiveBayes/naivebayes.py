0bcb4bc75016916a1cf43736ab3fd0c037a4eca5
LeiBaoCUB <lei.bao@colorado.edu>
2014-02-24 18:12:56 -0700

    clean and modified naivebayes.py can use lidstone function

--- naiveBayes/naivebayes.py
+++ naiveBayes/naivebayes.py
@@ -5,26 +5,12 @@ import numpy as np                                                          #imp
 import nltk
 from sklearn import cross_validation
 from nltk.probability import *
+from collections import defaultdict
 
-train = pd.read_csv("../data/train.csv")                                       #load train.csv as a pandas frame
+def preprocessQuery(train):
+    train['query'] = train['query'].apply(lambda x: x.upper())
+    return train
 
-totalnum = len(train)
-print "Number of Rows: " + str(len(train))                                  #prints the total number of rows
-print "Number of Unique Users: " + str(len(train['user'].unique()))         #prints the number of unique users...
-print "Number of Unique SKU: " + str(len(train['sku'].unique()))
-#print "Number of Unique Categories: " + str(len(train['category'].unique()))
-
-datUser = train['user']
-datSKU = train['sku']
-datCat = train['category']
-
-datQuery = train['query']
-datQuery = datQuery.apply(lambda x: x.upper())
-
-datClickT = pd.to_datetime(train['click_time'])
-datQueryT = pd.to_datetime(train['query_time'])
-datDiff = (datClickT - datQueryT).apply(lambda x: x / np.timedelta64(1, 's'))  # change date time to float
-datDiff = datDiff / 300.0   # normalize to 1
 def nClassify(x,values):
     if x < values[0]:
         return 0
@@ -39,47 +25,108 @@ def nDivide(dat,n):
     perc = np.linspace(0,1,n+1)
     perc = perc[1:-1]
     for i in range(len(perc)):
-        vs[i] = datDiff.quantile(perc[i])
+        vs[i] = dat.quantile(perc[i])
     datD = dat.apply(nClassify,values=vs)         
-    return datD
+    return (datD,vs)
     
-datTime = nDivide(datDiff,4)
-datTime.name = 'time'
+def preprocessTime(train):
+    datClickT = pd.to_datetime(train['click_time'])
+    datQueryT = pd.to_datetime(train['query_time'])
+    datDiff = (datClickT - datQueryT).apply(lambda x: x / np.timedelta64(1, 's'))  # change date time to float
+    datDiff = datDiff / 300.0   # normalize to 1
+    (datTime,vs) = nDivide(datDiff,4)
+#    datTime.name = 'time'
+    train['time'] = datTime
+    return train
 
-newData = pd.concat([datUser,datQuery,datTime],axis=1)
-features = newData.columns
-featuresE = enumerate(features)
+class ModNaiveBayesClassifier(nltk.NaiveBayesClassifier):
+    @staticmethod   
+    def train(labeled_featuresets, estimator=ELEProbDist):
+        label_freqdist = FreqDist()
+        feature_freqdist = defaultdict(FreqDist)
+        feature_values = defaultdict(set)
+        fnames = set()
+        
+        # Count up how many times each feature value occurred, given
+        # the label and featurename.
+        for featureset, label in labeled_featuresets:
+            label_freqdist.inc(label)
+            for fname, fval in featureset.items():
+                # Increment freq(fval|label, fname)
+                feature_freqdist[label, fname].inc(fval)
+                # Record that fname can take the value fval.
+                feature_values[fname].add(fval)
+                # Keep a list of all feature names.
+                fnames.add(fname)
+        
+        # If a feature didn't have a value given for an instance, then
+        # we assume that it gets the implicit value 'None.'  This loop
+        # counts up the number of 'missing' feature values for each
+        # (label,fname) pair, and increments the count of the fval
+        # 'None' by that amount.
+        for label in label_freqdist:
+            num_samples = label_freqdist[label]
+            for fname in fnames:
+                count = feature_freqdist[label, fname].N()
+                feature_freqdist[label, fname].inc(None, num_samples-count)
+                feature_values[fname].add(None)
+        
+        # Create the P(label) distribution
+        label_probdist = estimator(label_freqdist,bins = len(label_freqdist))
+        
+        # Create the P(fval|label, fname) distribution
+        feature_probdist = {}
+        for ((label, fname), freqdist) in feature_freqdist.items():
+            probdist = estimator(freqdist, bins=len(feature_values[fname])) 
+            feature_probdist[label,fname] = probdist
+        
+        return ModNaiveBayesClassifier(label_probdist, feature_probdist)
+ 
+def lidstone(gamma):
+    return lambda fd, bins: LidstoneProbDist(fd, gamma, bins)               
+        
+def main():
+    train = pd.read_csv("../data/train.csv")                                       #load train.csv as a pandas frame
+    train = preprocessQuery(train)
+    
+    train = preprocessTime(train)
 
-d = [(dict([(colname,row[i])
-    for i,colname in enumerate(features)
+    features=['time','user','query']
+    label = 'sku'
+#newData = pd.concat([datUser,datQuery,datTime],axis=1)
+    featuresE = set((j,item) for j,item in enumerate(train.columns) if item in features)
+
+    labeled_f = [(dict([(colname, row[i])
+    for i,colname in featuresE
     ]),
-    datSKU[j]) 
-    for j,row in enumerate(newData.values)
+    train.loc[j,label]) 
+    for j,row in enumerate(train.values)
     ]  
+    classifier = ModNaiveBayesClassifier.train(labeled_f,estimator=MLEProbDist)
+    ##estimator choices:ELEProbDist, LaplaceProbDist,LidstoneProbDist,MLEProbDist,ConditionalProbDist,
+    #
+    testDat = labeled_f[0:1000]
+    print "using MLE, accuracy", nltk.classify.accuracy(classifier,testDat)  
+    print classifier.show_most_informative_features(5)        
 
-labeled_f = d
-#gt = lambda fd, bins: SimpleGoodTuringProbDist(fd, bins=1e5)
-classifier = nltk.NaiveBayesClassifier.train(labeled_f,estimator=MLEProbDist)
-#estimator choices:ELEProbDist, LaplaceProbDist,LidstoneProbDist,MLEProbDist,ConditionalProbDist,
+    est = lidstone(0.1)
+    est = WittenBellProbDist
+    classifier = ModNaiveBayesClassifier.train(labeled_f,estimator=est)
+    print "using lidstone(0.1), accuracy", nltk.classify.accuracy(classifier,testDat) 
+    print classifier.show_most_informative_features(5)     
+    #
+    cv = cross_validation.KFold(n=len(labeled_f),n_folds=10,indices=True,shuffle=False, random_state=None, k=None)
+    #
+    scores=[]
+    for traincv, testcv in cv:
+        classif = ModNaiveBayesClassifier.train([labeled_f[i] for i in traincv], estimator = est)
+        score = nltk.classify.accuracy(classif, [labeled_f[i] for i in testcv])
+        scores.append(score)
+        print 'accuracy:',  score 
+    
+    print sum(scores)/len(scores)
 
-testDat = d[0:1000]
-print nltk.classify.accuracy(classifier,testDat)  
-#print classifier.show_most_informative_features()        
-#def lidstone(gamma):
-#    return lambda fd, bins: LidstoneProbDist(fd, gamma, bins)
-#est = lidstone(0.1)
-#classifier = nltk.NaiveBayesClassifier.train(labeled_f,estimator=est)
-
-cv = cross_validation.KFold(n=len(labeled_f),n_folds=10,indices=True,shuffle=True, random_state=None, k=None)
-
-scores=[]
-for traincv, testcv in cv:
-#    classif = nltk.NaiveBayesClassifier.train(labeled_f[traincv[0]:traincv[len(traincv)-1]],
-#                                               estimator=MLEProbDist)
-#    print 'accuracy:', nltk.classify.accuracy(classif, labeled_f[testcv[0]:testcv[len(testcv)-1]])
-    classif = nltk.NaiveBayesClassifier.train([labeled_f[i] for i in traincv], estimator = LaplaceProbDist)
-    score = nltk.classify.accuracy(classif, [labeled_f[i] for i in testcv])
-    scores.append(score)
-    print 'accuracy:',  score 
-
-print sum(scores)/len(scores)
+####################
+# run main() as the main function
+if __name__=='__main__':
+    main()
