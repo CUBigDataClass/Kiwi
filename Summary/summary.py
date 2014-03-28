903fca1c0a6df77218ffea5b8e90cdf0b65ac8d3
Gregory Petropoulos <gregpetrop@gmail.com>
2014-03-27 23:50:04 -0600

    added notebooks from exploration, review analysis, and processing for query

--- Summary/summary.py
+++ Summary/summary.py
@@ -3,9 +3,16 @@
 import pandas as pd                                                         #import pandas
 import numpy as np                                                          #import numpy
 
-train = pd.read_csv("data/train.csv")                                       #load train.csv as a pandas frame
+train = pd.read_csv("../data/train.csv")                                       #load train.csv as a pandas frame
 
 print "Number of Rows:\t\t\t" + str(len(train))                                  #prints the total number of rows
 print "Number of Unique Users:\t\t" + str(len(train['user'].unique()))         #prints the number of unique users...
 print "Number of Unique SKU:\t\t" + str(len(train['sku'].unique()))
 print "Number of Unique Categories:\t" + str(len(train['category'].unique()))
+
+testisnull = pd.isnull(train['sku'])
+
+for i in testisnull:
+  if (i == True):
+    print "null"
+
